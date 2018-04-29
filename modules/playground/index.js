758afec3214b71bf925540ba76401a408c781f3b
yamatatsu <the.kinnikuman@gmail.com>
2018-04-30 03:13:51 +0900

    遊び

--- modules/playground/index.js
+++ modules/playground/index.js
@@ -1,22 +1,27 @@
 function getTokenizer() {
-  const kuromoji = require('kuromoji')
-  const builder = kuromoji.builder({ dicPath: path.resolve('node_modules', 'kuromoji', 'dict') })
+  const kuromoji = require('kuromoji');
+  const builder = kuromoji.builder({
+    dicPath: path.resolve('node_modules', 'kuromoji', 'dict'),
+  });
   return new Promise((resolve, reject) => {
     builder.build((err, tokenizer) => {
       if (err) {
-        reject(err)
+        reject(err);
       } else {
-        resolve(tokenizer)
+        resolve(tokenizer);
       }
-    })
-  })
+    });
+  });
 }
 
-const kuromoji = require('kuromoji')
+const kuromoji = require('kuromoji');
+
 kuromoji
   .builder({ dicPath: path.resolve('node_modules', 'kuromoji', 'dict') })
-  .build((err, tokenizer) => (
-    console.log(tokenizer.tokenize("頑張っていこうぜ"))
-  ))
+  .build((err, tokenizer) =>
+    console.log(tokenizer.tokenize('頑張っていこうぜ')),
+  );
 
-  tokens.map(t => t.basic_form).reduce((acc, w) => [[w, acc[0] ? acc[0][1]+1 : 0], ...acc], [])
+tokens
+  .map(t => t.basic_form)
+  .reduce((acc, w) => [[w, acc[0] ? acc[0][1] + 1 : 0], ...acc], []);
