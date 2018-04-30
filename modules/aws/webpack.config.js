78dcea739b89a20857b2a5dcaaedeeae6afadd95
yamatatsu <the.kinnikuman@gmail.com>
2018-05-01 00:37:39 +0900

    cfn lambda の気持ちが分かってきた

--- modules/aws/webpack.config.js
+++ modules/aws/webpack.config.js
@@ -1,9 +1,11 @@
+const path = require('path');
+
 exports.default = {
   entry: './src/index.js',
   target: 'node',
   output: {
-    path: __dirname,
-    filename: './bundle.js',
+    path: path.resolve(__dirname, 'dist', 'lambda'),
+    filename: 'bundle.js',
     libraryTarget: 'commonjs2',
   },
   devtool: 'inline-source-map',
