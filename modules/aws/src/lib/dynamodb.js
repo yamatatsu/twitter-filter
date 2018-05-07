6c2fda223d6154734e47548720de94035e2315cd
yamatatsu <the.kinnikuman@gmail.com>
2018-05-08 02:10:03 +0900

    refactor

--- modules/aws/src/lib/dynamodb.js
+++ modules/aws/src/lib/dynamodb.js
@@ -1,79 +1,66 @@
 const util = require("util");
 const AWS = require("aws-sdk");
 
-const documentClient = new AWS.DynamoDB.DocumentClient();
+const client = createPromisedClient();
 
 function batchPutToRawTweets(tweets) {
-  return new Promise((resolve, reject) => {
-    documentClient.batchWrite(
-      {
-        RequestItems: {
-          TwitterFilter_RawTweets: tweets.map(t => ({
-            PutRequest: {
-              Item: {
-                TweetId: t.id_str,
-                createdAt: t.created_at,
-                text: t.text,
-                userId: t.user.id,
-                userName: t.user.name,
-                userScreenName: t.user.screen_name,
-              },
-            },
-          })),
+  return client("batchWrite", {
+    RequestItems: {
+      TwitterFilter_RawTweets: tweets.map(t => ({
+        PutRequest: {
+          Item: {
+            TweetId: t.id_str,
+            createdAt: t.created_at,
+            text: t.text,
+            userId: t.user.id,
+            userName: t.user.name,
+            userScreenName: t.user.screen_name,
+          },
         },
-      },
-      (err, data) => {
-        if (err) {
-          const message = util.format("dynamodb batchPutToRawTweets. %o", {
-            err,
-            tweets,
-          });
-          reject(message);
-        } else {
-          resolve(data);
-        }
-      }
-    );
-  });
+      })),
+    },
+  }).catch(handleErr("dynamodb batchPutToRawTweets. %o", { tweets }));
 }
 
 function putToKVS(key, value) {
-  return new Promise((resolve, reject) => {
-    documentClient.put(
-      { TableName: "TwitterFilter_KVS", Item: { Key: key, Value: value } },
-      (err, data) => {
-        if (err) {
-          const message = util.format("dynamodb putToKVS. %o", {
-            err,
-            key,
-            value,
-          });
-          reject(message);
-        } else {
-          resolve(data);
-        }
-      }
-    );
-  });
+  return client("put", {
+    TableName: "TwitterFilter_KVS",
+    Item: { Key: key, Value: value },
+  }).catch(handleErr("dynamodb putToKVS. %o", { key, value }));
 }
 
 function getValueFromKVS(key) {
-  return new Promise((resolve, reject) => {
-    documentClient.get(
-      { TableName: "TwitterFilter_KVS", Key: { Key: key } },
-      (err, data) => {
-        if (err) {
-          const message = util.format("dynamodb getValueFromKVS. %o", {
-            err,
-            key,
-          });
-          reject(message);
-        } else {
-          resolve(data.value);
-        }
-      }
-    );
-  });
+  return client("get", {
+    TableName: "TwitterFilter_KVS",
+    Key: { Key: key },
+  })
+    .then(data => data.value)
+    .catch(handleErr("dynamodb getValueFromKVS. %o", { key }));
 }
 
 module.exports = { batchPutToRawTweets, putToKVS, getValueFromKVS };
+
+// /////////
+// private
+
+function createPromisedClient() {
+  const documentClient = new AWS.DynamoDB.DocumentClient();
+
+  return (func, params) =>
+    new Promise((resolve, reject) => {
+      documentClient[func](
+        params,
+        (err, data) => (err ? reject(err) : resolve(data))
+      );
+    });
+}
+
+function handleErr(message, obj) {
+  return err =>
+    Promise.reject(
+      util.format(message, {
+        err,
+        ...obj,
+      })
+    );
+}
