a4c41e0088c6ba3b5174e2a4f63c61d734944f51
yamatatsu <the.kinnikuman@gmail.com>
2018-05-26 23:03:26 +0900

    remove comment-out-line

--- modules/aws/src/handlers/create-model.js
+++ modules/aws/src/handlers/create-model.js
@@ -3,24 +3,8 @@ const { SVM } = require("svm");
 const db = require("../lib/dynamodb");
 const s3 = require("../lib/s3");
 
-// const dataToWordsList = data => data.Items.map(item => item.words);
-// const dataToLabels = data => data.Items.map(item => item.label);
-// const getWordsListAndLabels = data =>
-//   data.Items.reduce(
-//     (acc, { words, label }) => {
-//       if (!words || !label) return acc;
-//       const [accVector, accLabels] = acc;
-//       return [
-//         [...accVector, wordsToVector(words, dict)],
-//         accLabels.concat(label),
-//       ];
-//     },
-//     [[], []]
-//   );
 const wordsToVector = (words, dict) =>
   dict.map(w => (words.indexOf(w) === -1 ? 0 : 1));
-// const createVectors = (wordsList, dict) =>
-//   wordsList.map(words => wordsToVector(words, dict));
 
 const concatVecAndLabel = (acc, vec, label) => {
   const [accVector, accLabels] = acc;
@@ -33,11 +17,6 @@ const recurse = (dict, lastEvaluatedKey, vectors = [], labels = []) =>
       IndexName: "ByLabel",
     })
     .then(data => {
-      // const wordsList = dataToWordsList(data);
-      // const newLabels = labels.concat(dataToLabels(data));
-
-      // const newVectors = vectors.concat(createVectors(wordsList, dict));
-
       const [newVectors, newLabels] = data.Items.reduce(
         (acc, { words, label }) =>
           words && label
