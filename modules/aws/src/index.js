eb80830980a98143094bc098b6d3ccf4eefe9b8e
yamatatsu <the.kinnikuman@gmail.com>
2018-05-28 13:35:46 +0900

    remove: 使ってないfanction

--- modules/aws/src/index.js
+++ modules/aws/src/index.js
@@ -1,5 +1,4 @@
 exports.fetchTweets = require("./handlers/fetch-tweets");
 exports.textToWords = require("./handlers/text-to-words");
-exports.textToWordsAll = require("./handlers/text-to-words_all");
 exports.wordsToDict = require("./handlers/words-to-dict");
 exports.createModel = require("./handlers/create-model");
