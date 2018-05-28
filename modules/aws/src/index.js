2127867e494a80857d8bc8a69469d6af8c03bd6f
yamatatsu <the.kinnikuman@gmail.com>
2018-05-28 14:21:53 +0900

    remove: 使ってないfunction

--- modules/aws/src/index.js
+++ modules/aws/src/index.js
@@ -1,4 +1,3 @@
 exports.fetchTweets = require("./handlers/fetch-tweets");
-exports.textToWords = require("./handlers/text-to-words");
 exports.wordsToDict = require("./handlers/words-to-dict");
 exports.createModel = require("./handlers/create-model");
